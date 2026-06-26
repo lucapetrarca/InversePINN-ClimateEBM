eb1a80c7f26d9d30c2245ca2e4520a025b78b883
lucapetrarca <lucapetrarca1@gmail.com>
2026-06-26 17:56:02 -0300

    Agrego model.py

---
+++ src/model.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+class ClimateInversePINN(nn.Module):
+    def __init__(self, hidden_layers=4, neurons_per_layer=32):
+        super(ClimateInversePINN, self).__init__()
+        #Input: x (seno de la latitud, 1D) -> Output: T (Temperatura en Kelvin, 1D)
+        layers = []
+        layers.append(nn.Linear(1, neurons_per_layer))
+        layers.append(nn.Tanh())
+        
+        for _ in range(hidden_layers - 1):
+            layers.append(nn.Linear(neurons_per_layer, neurons_per_layer))
+            layers.append(nn.Tanh())
+            
+        layers.append(nn.Linear(neurons_per_layer, 1))
+        
+        self.net = nn.Sequential(*layers)
+        
+        #Parámetros Físicos a descubrir
+        
+        #Coeficiente "D" de difusión de calor
+        self.D = nn.Parameter(torch.tensor([0.5], dtype=torch.float32))
+        
+        #Constantes de radiación saliente (R_out = A + B*T)
+        self.A_out = nn.Parameter(torch.tensor([200.0], dtype=torch.float32))
+        self.B_out = nn.Parameter(torch.tensor([1.5], dtype=torch.float32))
+        
+    def forward(self, x):
+        #Se calcula la temperatura a partir de un valor de x
+        return self.net(x)
